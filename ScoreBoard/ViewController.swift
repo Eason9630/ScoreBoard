ba2a806d25bab47b8a585a364fd80cd65678f787
Eason <Eason9630@gmail.com>
2023-03-26 08:34:19 +0800

    Initial Commit

---
+++ ScoreBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoreBoard
+//
+//  Created by 林祔利 on 2023/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
